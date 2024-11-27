a132368dcb2858cd99503898b7e9e861917a5b63
e.letos <e.letos@levi9.com>
2024-11-27 16:45:36 +0200

    Add Category, User and Role

--- expense_tracker/src/main/java/com/expensetracker/expense_tracker/entity/PaymentType.java
+++ expense_tracker/src/main/java/com/expensetracker/expense_tracker/entity/PaymentType.java
@@ -1,24 +1,5 @@
 package com.expensetracker.expense_tracker.entity;
 
 public enum PaymentType {
-    CREDIT_CARD("Credit Card"),
-    DEBIT_CARD("Debit Card"),
-    BANK_TRANSFER("Bank Transfer"),
-    CASH("Cash"),
-    CRYPTO("Cryptocurrency");
-
-    private final String description;
-
-    PaymentType(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public String toString() {
-        return description;
-    }
+    CASH, CREDIT_CARD, DEBIT_CARD, BANK_TRANSFER, GIFT_CARD, CRYPTOCURRENCY
 }
---
+++ expense_tracker/src/main/java/com/expensetracker/expense_tracker/entity/Role.java
@@ -0,0 +1,5 @@
+package com.expensetracker.expense_tracker.entity;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_USER
+}
